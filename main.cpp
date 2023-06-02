e3ce0d1831d78beb2ffc28f07185c541f80cfa3b
Matheus <127265280+MatheusHMafra@users.noreply.github.com>
2023-06-01 21:57:02 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,66 +4,72 @@ using namespace std;
 #define TAM 9
 
 int main(){
+    srand(time(NULL));
+    int linha = 0, coluna = 0;
+    int jafoi = 0, jafoi1 = 0;
     int matA[TAM][TAM] = {4,9,5,2,8,7,3,6,1,7,2,8,6,1,3,4,9,5,3,6,1,9,5,4,7,2,8,6,5,3,8,4,9,2,1,7,9,8,4,1,7,2,6,5,3,2,1,7,5,3,6,9,8,4,1,3,2,4,6,5,8,7,9,5,4,6,7,9,8,1,3,2,8,7,9,3,2,1,5,4,6};
+    int matB[TAM][TAM]={0};
     cout << "\t Sudoku: "<< endl;
-    for (int i = 0; i < TAM; i++){
-        if (i % 3 ==0 or i % 6 ==0){
+    for (linha = 0; linha < TAM; linha++){
+        if (linha % 3 ==0 or linha % 6 ==0){
             cout << " -----------------------" << endl;
         }
-        for (int j = 0; j < TAM; j++){
-            if (j % 3 ==0){
+        for (coluna = 0; coluna < TAM; coluna++){
+            if (coluna % 3 ==0){
                 cout << "| ";
             }
-            cout << matA[i][j] << " ";
+            cout << matA[linha][coluna] << " ";
         }
         cout << "|";
         cout << endl;
     }
     cout << " -----------------------" << endl;
     cout << "       Transposta: "<< endl;
-    for (int i = 0; i < TAM; i++){
-        if (i % 3 ==0 or i % 6 ==0){
+    for (linha = 0; linha < TAM; linha++){
+        if (linha % 3 ==0 or linha % 6 ==0){
             cout << " -----------------------" << endl;
         }
-        for (int j = 0; j < TAM; j++){
-            if (j % 3 ==0){
+        for (coluna = 0; coluna < TAM; coluna++){
+            if (coluna % 3 ==0){
                 cout << "| ";
             }
-            cout << matA[j][i] << " ";
+            cout << matA[coluna][linha] << " ";
         }
         cout << "|";
         cout << endl;
     }
-    cout << " -----------------------" << endl;
-        cout << "  Invertida por linha: "<< endl;
-    for (int i = 9; i > 0; i--){
-        if (i % 3 ==0 or i % 6 ==0){
-            cout << " -----------------------" << endl;
+    /*for (int z = 0; z < 41; z++){
+        linha = rand()%9+1;
+        coluna = rand()%9+1;
+        if (jafoi == linha){
+            linha = rand()%9+1;
+        } else if (jafoi1 == coluna){
+            coluna = rand()%9+1;
         }
-        for (int j = 0; j < TAM; j++){
-            if (j % 3 ==0){
+        jafoi = linha;
+        jafoi1 = coluna;
+        matB[linha][coluna] = matA[coluna][linha];
+    }*/
+    for(int z =0; z<41; z++){
+        if(matB[linha][coluna]==0){
+            linha = rand()%9+1;
+            coluna = rand()%9+1;
+            matB[linha][coluna]=matA[linha][coluna];
+        }
+    }
+    cout<<endl;
+    for (linha = 0; linha < TAM; linha++){
+            if (linha % 3 ==0 or linha % 6 ==0){
+                cout << " -----------------------" << endl;
+            }
+            for (coluna = 0; coluna < TAM; coluna++){
+            if (coluna % 3 ==0){
                 cout << "| ";
             }
-            cout << matA[i-1][j] << " ";
+            cout << matB[linha-1][coluna] << " ";
         }
         cout << "|";
         cout << endl;
     }
-    cout << " -----------------------" << endl;
-    cout << "  Invertida por coluna: "<< endl;
-    for (int i = 0; i < TAM; i++){
-        if (i % 3 ==0 or i % 6 ==0){
-            cout << " -----------------------" << endl;
-        }
-        for (int j = 9; j > 0; j--){
-            if (j % 3 ==0){
-                cout << "| ";
-            }
-            cout << matA[i][j-1] << " ";
-        }
-        cout << "|";
-        cout << endl;
-    }
-    cout << " -----------------------" << endl;
     return 0;
 }

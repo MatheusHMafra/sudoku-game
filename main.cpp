e444ad3a0adc1f84168603a32908f2edfacd3c86
Matheus <127265280+MatheusHMafra@users.noreply.github.com>
2023-06-01 21:27:20 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,11 @@
 #include <iostream>
 using namespace std;
 
+#define TAM 9
+
 int main(){
-    srand(time(NULL));
-    int TAM = 9, matA[TAM][TAM], temp;
-    for (int i = 0; i < TAM; i++) {
-        for (int j = 0; j < TAM; j++) {
-            matA[i][j] = 1;
-            matA[i+1][j+1] = 2;
-        }
-    }
-    cout << "Matriz A: "<< endl;
+    int matA[TAM][TAM] = {4,9,5,2,8,7,3,6,1,7,2,8,6,1,3,4,9,5,3,6,1,9,5,4,7,2,8,6,5,3,8,4,9,2,1,7,9,8,4,1,7,2,6,5,3,2,1,7,5,3,6,9,8,4,1,3,2,4,6,5,8,7,9,5,4,6,7,9,8,1,3,2,8,7,9,3,2,1,5,4,6};
+    cout << "\t Sudoku: "<< endl;
     for (int i = 0; i < TAM; i++){
         if (i % 3 ==0 or i % 6 ==0){
             cout << " -----------------------" << endl;
@@ -20,6 +15,51 @@ int main(){
                 cout << "| ";
             }
             cout << matA[i][j] << " ";
+        }
+        cout << "|";
+        cout << endl;
+    }
+    cout << " -----------------------" << endl;
+    cout << "       Transposta: "<< endl;
+    for (int i = 0; i < TAM; i++){
+        if (i % 3 ==0 or i % 6 ==0){
+            cout << " -----------------------" << endl;
+        }
+        for (int j = 0; j < TAM; j++){
+            if (j % 3 ==0){
+                cout << "| ";
+            }
+            cout << matA[j][i] << " ";
+        }
+        cout << "|";
+        cout << endl;
+    }
+    cout << " -----------------------" << endl;
+        cout << "  Invertida por linha: "<< endl;
+    for (int i = 9; i > 0; i--){
+        if (i % 3 ==0 or i % 6 ==0){
+            cout << " -----------------------" << endl;
+        }
+        for (int j = 0; j < TAM; j++){
+            if (j % 3 ==0){
+                cout << "| ";
+            }
+            cout << matA[i-1][j] << " ";
+        }
+        cout << "|";
+        cout << endl;
+    }
+    cout << " -----------------------" << endl;
+    cout << "  Invertida por coluna: "<< endl;
+    for (int i = 0; i < TAM; i++){
+        if (i % 3 ==0 or i % 6 ==0){
+            cout << " -----------------------" << endl;
+        }
+        for (int j = 9; j > 0; j--){
+            if (j % 3 ==0){
+                cout << "| ";
+            }
+            cout << matA[i][j-1] << " ";
         }
         cout << "|";
         cout << endl;

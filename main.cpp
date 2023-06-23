d6abb896a2926edd567d92c5f858ee77947e76f4
Matheus <127265280+MatheusHMafra@users.noreply.github.com>
2023-06-22 21:52:17 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,99 +5,96 @@ using namespace std;
 
 int main() {
     srand(time(NULL));
-    int linha = 0, coluna = 0;
-    int matA[TAM][TAM] = {4,9,5,2,8,7,3,6,1,7,2,8,6,1,3,4,9,5,3,6,1,9,5,4,7,2,8,6,5,3,8,4,9,2,1,7,9,8,4,1,7,2,6,5,3,2,1,7,5,3,6,9,8,4,1,3,2,4,6,5,8,7,9,5,4,6,7,9,8,1,3,2,8,7,9,3,2,1,5,4,6};
-    int matB[TAM][TAM] = {0};
-
-    cout << "\t Sudoku: " << endl; //Gabarito
-    for (linha = 0; linha < TAM; linha++) {
-        if (linha % 3 == 0 || linha % 6 == 0) {
-            cout << " -----------------------" << endl;
-        }
-        for (coluna = 0; coluna < TAM; coluna++) {
-            if (coluna % 3 == 0) {
-                cout << "| ";
-            }
-            cout << matA[linha][coluna] << " ";
-        }
-        cout << "|";
-        cout << endl;
-    }
-    cout << " -----------------------" << endl;
+    int linha=0, coluna=0, option=0, option1=0;
+    int principal[TAM][TAM] = {4,9,5,2,8,7,3,6,1,7,2,8,6,1,3,4,9,5,3,6,1,9,5,4,7,2,8,6,5,3,8,4,9,2,1,7,9,8,4,1,7,2,6,5,3,2,1,7,5,3,6,9,8,4,1,3,2,4,6,5,8,7,9,5,4,6,7,9,8,1,3,2,8,7,9,3,2,1,5,4,6};
+    int gabarito[TAM][TAM];
+    int jogo[TAM][TAM];
     
-    cout << "       Transposta: "<< endl;
-    for (int i = 0; i < TAM; i++){
-        if (i % 3 ==0 or i % 6 ==0){
-            cout << " -----------------------" << endl;
+    do {
+        cout << "--------------------------------------------------------" << endl;
+        cout << "--------------------------------------------------------" << endl;
+        cout << "                       Sudoku" << endl;
+        cout << "                  Escolha uma opcão: " << endl << endl;
+        cout << "                     1 - Jogar" << endl;
+        cout << "                     2 - Sobre" << endl;
+        cout << "                     3 - Sair" << endl;
+        cout << "--------------------------------------------------------" << endl;
+        cout << "--------------------------------------------------------" << endl;
+        cin >> option;
+        switch (option){
+            case 1:
+                option1 = rand()%4+1;
+                cout << option1;
+                if (option1 == 1){
+                    for (linha = 0; linha < TAM; linha++) { // MATRIZ GABARITO
+                        for (coluna = 0; coluna < TAM; coluna++) {
+                            gabarito[linha][coluna] = principal[linha][coluna];
+                        }
+                    }
+                }else if (option1 == 2){
+                    for (linha = 0; linha < TAM; linha++){ // MATRIZ TRANSPOSTA
+                        for (coluna = 0; coluna < TAM; coluna++){
+                            gabarito[linha][coluna] = principal[coluna][linha];
+                        }
+                    }
+                }else if (option1 == 3){
+                    for (int linha = 9; linha > 0; linha--){ // MATRIZ INVERTIDA POR LINHA 
+                        for (int coluna = 0; coluna < TAM; coluna++){
+                            gabarito[linha][coluna] = principal[linha-1][coluna];
+                        }
+                    }
+                }else if (option1 == 4){
+                    for (int linha = 0; linha < TAM; linha++){ // MATRIZ INVERTIDA POR COLUNA
+                        for (int coluna = 9; coluna > 0; coluna--){
+                            gabarito[linha][coluna] = principal[linha][coluna-1];
+                        }
+                    }
+                }
+                for (int z = 0; z < 41; z++) {
+                    do {
+                        linha = rand() % TAM;
+                        coluna = rand() % TAM;
+                    } while (jogo[linha][coluna] != 0);
+                    jogo[linha][coluna] = gabarito[linha][coluna];
+                }
+                cout << endl;
+                for (linha = 0; linha < TAM; linha++) {
+                    if (linha % 3 == 0 || linha % 6 == 0) {
+                        cout << " -----------------------" << endl;
+                    }
+                    for (coluna = 0; coluna < TAM; coluna++) {
+                        if (coluna % 3 == 0) {
+                            cout << "| ";
+                        }
+                        cout << jogo[linha][coluna] << " ";
+                    }
+                    cout << "|";
+                    cout << endl;
+                }
+                cout << " -----------------------" << endl;
+                
+                
+                
+                
+                break;
+            case 2:
+                system("clear");
+                cout << "Equipe de desenvolvimento: Leonardo Borges, Lucas Barcia e Matheus Mafra" << endl;
+                cout << "Mês/ano: Junho/2023" << endl;
+                cout << "Professor: Rafael Ballottin" << endl;
+                cout << "Disciplina: Algoritmos e Programação" << endl;
+                system("read 0 -p");
+                system("clear");
+                break;
+            default:
+                system("clear");
+                cout  << endl << "Opção invalida. Tente novamente." << endl;
+                system("read 0 -p");
+                system("clear");
+                break;
         }
-        for (int j = 0; j < TAM; j++){
-            if (j % 3 ==0){
-                cout << "| ";
-            }
-            cout << matA[j][i] << " ";
-        }
-        cout << "|";
-        cout << endl;
-    }
-    cout << " -----------------------" << endl;
-    
-    cout << "  Invertida por linha: "<< endl;
-    for (int i = 9; i > 0; i--){
-        if (i % 3 ==0 or i % 6 ==0){
-            cout << " -----------------------" << endl;
-        }
-        for (int j = 0; j < TAM; j++){
-            if (j % 3 ==0){
-                cout << "| ";
-            }
-            cout << matA[i-1][j] << " ";
-        }
-        cout << "|";
-        cout << endl;
-    }
-    cout << " -----------------------" << endl;
-    
-    cout << "  Invertida por coluna: "<< endl;
-    for (int i = 0; i < TAM; i++){
-        if (i % 3 ==0 or i % 6 ==0){
-            cout << " -----------------------" << endl;
-        }
-        for (int j = 9; j > 0; j--){
-            if (j % 3 ==0){
-                cout << "| ";
-            }
-            cout << matA[i][j-1] << " ";
-        }
-        cout << "|";
-        cout << endl;
-    }
-    cout << " -----------------------" << endl;
-    /*
-    for (int z = 0; z < 41; z++) {
-        do {
-            linha = rand() % TAM;
-            coluna = rand() % TAM;
-        } while (matB[linha][coluna] != 0);
-
-        matB[linha][coluna] = matA[linha][coluna];
-    }
-    
-    cout << endl;
-    for (linha = 0; linha < TAM; linha++) {
-        if (linha % 3 == 0 || linha % 6 == 0) {
-            cout << " -----------------------" << endl;
-        }
-        for (coluna = 0; coluna < TAM; coluna++) {
-            if (coluna % 3 == 0) {
-                cout << "| ";
-            }
-            cout << matB[linha][coluna] << " ";
-        }
-        cout << "|";
-        cout << endl;
-    }
-    cout << " -----------------------" << endl;
-    */
-
+    } while (option != 3);
+    system("clear");
+    cout  << endl << "Obrigado por jogar! Até a proxima." << endl;
     return 0;
 }
